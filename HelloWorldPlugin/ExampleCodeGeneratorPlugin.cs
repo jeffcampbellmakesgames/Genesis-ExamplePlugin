14755f0ec4f2539c87ca95cc29a4315b23c998a7
Jeff Campbell <jeff@zapdot.com>
2021-06-14 09:00:27 -0400

    Created example code-generation plugin

---
+++ HelloWorldPlugin/ExampleCodeGeneratorPlugin.cs
@@ -0,0 +1,36 @@
+﻿using Genesis.Plugin;
+
+namespace HelloWorldPlugin
+{
+    /// <summary>
+    /// An example code generation plugin that creates a single output class file.
+    /// </summary>
+    public class ExampleCodeGeneratorPlugin : ICodeGenerator
+    {
+        /// <inheritdoc />
+        public string Name => nameof(ExampleCodeGeneratorPlugin);
+
+        /// <inheritdoc />
+        public int Priority => 0;
+
+        /// <inheritdoc />
+        public bool RunInDryMode => true;
+
+        private string EXAMPLE_FILE_CONTENTS =
+@"
+public class ExamplePluginOutputClass { }
+";
+
+        /// <inheritdoc />
+        public CodeGenFile[] Generate(CodeGeneratorData[] data)
+        {
+            return new CodeGenFile[]
+            {
+                new CodeGenFile(
+                    "ExamplePluginOutputClass.cs",
+                    EXAMPLE_FILE_CONTENTS,
+                    Name)
+            };
+        }
+    }
+}
